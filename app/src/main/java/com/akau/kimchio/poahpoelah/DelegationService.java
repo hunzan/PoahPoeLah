edf3afe8ca73bc5c0560faacbd60afefdf230db1
林阿猴 <hunzan@gmail.com>
2025-10-10 11:45:05 +0800

    Update pages and mute function

---
+++ app/src/main/java/com/akau/kimchio/poahpoelah/DelegationService.java
@@ -0,0 +1,14 @@
+package com.akau.kimchio.poahpoelah;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
